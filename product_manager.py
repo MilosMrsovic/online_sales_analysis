a84daacc955d15ff2f1e6e3bcff3b1f9b4f79a12
Milos Mrsovic <mrsovic72@gmail.com>
2025-02-07 12:39:55 +0100

    Dodavanje metode za uklanjanje proizvoda iz liste proizvoda

--- product_manager.py
+++ product_manager.py
@@ -14,3 +14,11 @@ class ProductManager:
     def total_sum(self):
         ukupno = sum(product.price * product.quantity for product in self.products)
         print(f"Ukupna cena svih proizvoda je: {ukupno:.2f} dinara")
+        
+    def product_removal(self):
+        product_remove = input("Uneti product za uklanjanje: ")
+        for product in self.products:
+            if product.name == product_remove:
+                self.products.remove(product)
+                print(f"Proizvod '{product_remove}' je uklonjen iz liste")
+                return
